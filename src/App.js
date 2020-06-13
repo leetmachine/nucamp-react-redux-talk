9b61b2da97f6ba5c9af02e98edcf02721c2e2ded
leetmachine <keegan_gladstone@datahouse.com>
2020-06-13 13:05:19 -0700

    add dashboard to app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
-<<<<<<< HEAD
-
-/*ACTIONS 
-if you intend to update the REDUX STORE
-all you need is a function that returns exactly:
-{ type: 'A_KEY_YOU_DEFINED', payload?: optional, any value you intend to use }
-*/
-function login() {
-  console.log('logging in...');
-  return {
-    type: 'LOGIN_USER',
-    payload: {name: 'Keegan'}
-  }
-}
-
-function logout() {
-  return {
-    type: 'LOGOUT_USER'
-  }
-}
-=======
 import Dashboard from './Dashboard';
->>>>>>> added-dashboard
 
 class App extends React.Component {
 
